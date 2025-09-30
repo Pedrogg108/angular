ba1aa79679156860d1a164ca6e9097b50928a102
Pedrogg108 <pedroalancay2018@gmail.com>
2025-09-30 16:49:25 -0300

    hice la pagina

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,11 @@ import { Contact } from './pages/contact/contact';
 import { Products } from './pages/products/products';
 
 export const routes: Routes = [
+    //definimos la ruta principal o raiz  en especifico que nos lleve a la ruta home
+    {path: "**",component:Home},
     {path: `home`, component: Home},
     {path: `about`,component: About},
     {path: `contact`,component:Contact},
     {path:`products`,component:Products}
-
 
 ];
