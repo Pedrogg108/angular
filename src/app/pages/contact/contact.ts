5956b48d9e0c41c31d08044b5e7672810d5f1e96
Pedrogg108 <pedroalancay2018@gmail.com>
2025-11-10 17:05:14 -0300

    terminado?

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.css'
+  styleUrls: ['./contact.css']
 })
 export class Contact {
+  // Definimos el formulario reactivo
+  formQueja = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    motivo: new FormControl('', Validators.required),
+    mensaje: new FormControl('', Validators.required)
+  });
 
+  // Método que se ejecuta al enviar el formulario
+  enviarQueja(): void {
+    if (this.formQueja.valid) {
+      console.log("Queja enviada con éxito:");
+      // datos individuales
+      console.log("Nombre:", this.formQueja.value.nombre);
+      console.log("Correo:", this.formQueja.value.correo);
+      console.log("Motivo:", this.formQueja.value.motivo);
+      console.log("Mensaje:", this.formQueja.value.mensaje);
+
+      // Limpia el formulario
+      this.formQueja.reset();
+    } 
+  }
 }
